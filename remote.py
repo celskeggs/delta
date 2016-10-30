57f2c592156ebae56b9952da62b993cb4492668e
Cel Skeggs <git@celskeggs.com>
2016-10-30 03:49:01 -0400

    Add a bunch more work

---
+++ remote.py
@@ -0,0 +1,36 @@
+from google.cloud import storage
+import tree
+
+paradox = None
+def get_ref():
+	global paradox
+	if paradox is None:
+		paradox = storage.Client(project="backups-cela").get_bucket("paradox_backup")
+	return paradox
+
+def list_upstream():
+	out = []
+	for blob in get_ref().list_blobs():
+		name = blob.name
+		if len(name) == tree.SHA_LEN and tree.is_hex(name):
+			out.append(name)
+	return out
+
+def upload_object(object, f, overwrite=False):
+	blob = get_ref().blob(object)
+	assert not blob.exists() or overwrite
+	if type(f) in (str, bytes):
+		blob.upload_from_string(f)
+	else:
+		blob.upload_from_file(f)
+
+def exists_object(object):
+	return get_ref().blob(object).exists()
+
+def download_object(object, f):
+	blob = get_ref().blob(object)
+	assert blob.exists()
+	if f is not None:
+		return blob.download_to_file(f)
+	else:
+		return blob.download_as_string()
--- tree.py
+++ tree.py
@@ -1,4 +1,6 @@
 import os
+import time
+import json
 import gnupg
 import hashlib
 
@@ -7,6 +9,7 @@ class DeltaException(Exception):
 
 DDIR = ".delta"
 KEYFILE = "key"
+TREEFILE = "tree"
 DATADIR = "data"
 STAGEDIR = "stage"
 CRYPTDIR = "crypt"
@@ -26,7 +29,7 @@ def find_ctx(fail=True):
 				raise DeltaException("Inconsistent state. Manual rescue required.")
 			with open(os.path.join(ddir_for(cur_dir), KEYFILE), "r") as f:
 				key_id = f.readline().strip()
-			if key_id not in [key["fingerprint"] for key in gpg.list_keys(True)]:
+			if not gpg.export_keys(key_id):
 				raise DeltaException("Key not found in keyring: %s" % key_id)
 			return cur_dir, ddir_for(cur_dir), key_id
 		cur_dir = os.path.dirname(cur_dir)
@@ -38,26 +41,29 @@ def find_ctx(fail=True):
 def fail(err):
 	raise err
 
-def tree_status(root):
-	link_count = 0
-	unsaved = []
+def tree_walk(root):
 	for dirpath, dirnames, filenames in os.walk(root, onerror=fail):
 		if DDIR in dirnames:
 			if dirpath != root:
 				raise DeltaException("Encountered %s file in subdirectory." % DDIR)
 			dirnames.remove(DDIR)
 		for filename in filenames:
-			filepath = os.path.join(dirpath, filename)
-			if os.path.islink(filepath):
-				link_count += 1
-			else:
-				assert os.path.exists(filepath), "failed: %s" % filepath
-				unsaved.append(filepath)
+			yield os.path.join(dirpath, filename)
+
+def tree_status(root):
+	link_count = 0
+	unsaved = []
+	for filepath in tree_walk(root):
+		if os.path.islink(filepath):
+			link_count += 1
+		else:
+			assert os.path.exists(filepath), "failed: %s" % filepath
+			unsaved.append(filepath)
 	unsaved.sort()
 	return link_count, unsaved
 
 def is_hex(x):
-	return all(c in "0123456789abcdefABCDEF" for c in x)
+	return all(c in "0123456789abcdef" for c in x)
 
 def cache_status(root):
 	cache_decrypt, cache_encrypt = [], []
@@ -94,7 +100,7 @@ def stash_file(root, file):
 	os.chmod(tempfile, os.stat(tempfile).st_mode & ~0o222)
 	assert not os.path.exists(file)
 	os.symlink(os.path.relpath(goalfile, os.path.dirname(os.path.abspath(file))), file)
-	os.utime(file, times=(os.path.getatime(tempfile), os.path.getmtime(tempfile)), follow_symlinks=False)
+	os.utime(file, ns=(os.path.getatime(tempfile), os.path.getmtime(tempfile)), follow_symlinks=False)
 	if os.path.exists(goalfile):
 		os.remove(tempfile)
 	else:
@@ -125,6 +131,16 @@ def decrypt_file(root, key, object):
 	assert object == sha256_file(tempfile)
 	os.rename(tempfile, target)
 
+def export_object(root, object):
+	source = os.path.join(ddir_for(root), CRYPTDIR, object)
+	assert os.path.exists(source)
+	return open(source, "rb")
+
+def import_object(root, object):
+	target = os.path.join(ddir_for(root), CRYPTDIR, object)
+	assert not os.path.exists(target)
+	return open(target, "wb")
+
 def init_folder(root, key):
 	ddir = ddir_for(root)
 	os.mkdir(ddir)
@@ -134,3 +150,73 @@ def init_folder(root, key):
 	os.mkdir(os.path.join(ddir, STAGEDIR))
 	os.mkdir(os.path.join(ddir, CRYPTDIR))
 	assert (root, ddir, key) == find_ctx()
+
+def get_tree(root):
+	tf = os.path.join(ddir_for(root), TREEFILE)
+	if not os.path.exists(tf):
+		return None
+	with open(tf, "r") as f:
+		return json.load(f)
+
+def set_tree(root, tree, crypt=False):
+	with open(os.path.join(ddir_for(root), TREEFILE), "w") as f:
+		json.dump(tree, f)
+
+def get_tree_crypt(root, key):
+	with open(os.path.join(ddir_for(root), TREEFILE), "rb") as f:
+		output = gpg.encrypt_file(f, [key], sign=key, armor=False, always_trust=True)
+	return output.data
+
+def set_tree_crypt(root, key, crypted):
+	output = gpg.decrypt(crypted, always_trust=True)
+	assert output.fingerprint == key, "key mismatch: unexpected %s" % decrypted.fingerprint
+	with open(os.path.join(ddir_for(root), TREEFILE), "wb") as f:
+		f.write(output.data)
+
+def dump_tree(root):
+	out = {}
+	for filename in tree_walk(root):
+		if not os.path.islink(filename):
+			continue
+		out[os.path.relpath(filename, root)] = [os.readlink(filename), os.path.getmtime(filename)]
+	return out
+
+def list_changes(root, tree):
+	tree2 = dump_tree(root)
+	for key, (link, mtime) in tree.items():
+		if key not in tree2:
+			if os.path.exists(os.path.join(root, key)):
+				yield "blocked", key, link, mtime
+			else:
+				yield "insert", key, link, mtime
+		elif tuple(tree2[key]) != (link, mtime):
+			print("MISMATCH", link, mtime, tree2[key])
+			yield "replace", key, link, mtime
+	for key, (link, mtime) in tree2.items():
+		if key not in tree:
+			yield "delete", key, link, mtime
+
+def apply_change(root, change):
+	cmd, key, link, mtime = change
+	path = os.path.join(root, key)
+	if cmd == "insert":
+		assert not os.path.exists(path)
+		os.makedirs(os.path.dirname(os.path.abspath(path)), exist_ok=True)
+		os.symlink(link, path)
+		os.utime(path, (time.time(), mtime))
+	elif cmd == "replace":
+		assert os.path.islink(path)
+		os.remove(path)
+		os.symlink(link, path)
+		os.utime(path, (time.time(), mtime))
+		leaf = os.path.dirname(os.path.abspath(path))
+		if not os.listdir(leaf):
+			os.removedirs(leaf)
+	elif cmd == "delete":
+		assert os.path.islink(path)
+		os.remove(path)
+		leaf = os.path.dirname(os.path.abspath(path))
+		if not os.listdir(leaf):
+			os.removedirs(leaf)
+	else:
+		assert False
